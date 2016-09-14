17b22f393faaa04250655ec1e6e58274eeba9581
Kevin Zhu <kzoo223@gmail.com>
2016-09-13 21:15:14 -0400

    function to pull objects from giphy using giphy API

--- js/app.js
+++ js/app.js
@@ -9,19 +9,61 @@ $(document).ready(function () {
     // Deletes previous items before adding new items
     $('#buttons').empty();
 
-    // Loops through the array of movies
+    // Loops through array 
     for (var i = 0; i < searchTerms.length; i++){
 
-      // Then dynamicaly generates buttons for each movie in the array
+      
 
-      // Note the jQUery syntax here... 
-        var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+      
+        var a = $('<button>')
         a.addClass('searchTerm'); // Added a class 
         a.attr('data-name', searchTerms[i]); // Added a data-attribute
-        a.text(searchTerms[i]); // Provided the initial button text
-        $('#buttons').append(a); // Added the button to the HTML
+        a.text(searchTerms[i]); // Provide button text
+        $('#buttons').append(a); // Add the button to the HTML
     }
   }
+
+
+  //function to pull data from API
+  function gifSearchDisplay(){
+    $("#gifArea").empty();
+    var type = ($(this).data("name"))
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q="+type+"&limit=10&api_key=dc6zaTOxFJmzC";
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function(response) {
+          
+          console.log(response)
+
+          result = response.data
+
+          for(i=0; result.length; i++){
+          
+          var itemDiv = $("<div>")
+          var gifItem = $("<img>")
+          gifItem.attr("src", response.data[i].images.fixed_height.url)
+          gifItem.attr("data-state", "animate")
+          gifItem.attr("data-still", response.data[i].images.fixed_height_still.url)
+          gifItem.attr("data-animate", response.data[i].images.fixed_height.url)
+          gifItem.attr("class", "giphy")
+
+          var rating = $("<p>")
+          rating.text("Rating: " + response.data[i].rating)
+
+          itemDiv.append(rating)
+          itemDiv.append(gifItem)
+
+          $("#gifArea").append(itemDiv)
+          
+
+          }
+    });
+  }
+
+
 
 renderButtons();
 
